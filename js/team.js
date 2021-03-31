d96b5ebe37b422525acb1d1715d9ac1289d78f95
Zadorin Artyom <superdeathcorpce@gmail.com>
2021-03-31 16:17:14 +0500

    Реализован функционал секции команды и отзывов

---
+++ js/team.js
@@ -0,0 +1,52 @@
+var team = $('.team');
+var teammates = $('.team__item');
+var arrowWidth = $('.team__arrow').css('borderTopWidth')
+
+var currIndex = null;
+
+
+teammates.on('click', function(e){
+  let thisIndex = $(this).index();
+
+  if(thisIndex === currIndex){
+    hideContent(thisIndex);
+    currIndex = null;
+    return;
+  }
+
+  if(currIndex !== null){
+    hideContent(currIndex);
+    currIndex = null;
+  }
+
+  showContent(thisIndex);
+  currIndex = thisIndex;
+})
+
+
+function hideContent(index){  
+  let elements = teammates.filter(`:nth-child(${index+1})`);
+  let arrows = elements.find('.team__arrow');
+
+  elements.find('.team__content').slideUp(time);
+
+  console.log(parseInt(arrowWidth))
+  arrows.animate({
+    borderBottomWidth: 0
+  },time/2).animate({
+    borderTopWidth: parseInt(arrowWidth)
+  },time/2)
+}
+
+function showContent(index){ 
+  let elements = teammates.filter(`:nth-child(${index+1})`);
+  let arrows = elements.find('.team__arrow');
+
+  elements.find('.team__content').slideDown(time);
+
+  arrows.animate({
+    borderTopWidth: 0
+  },time/2).animate({
+    borderBottomWidth: parseInt(arrowWidth)
+  },time/2)
+}
